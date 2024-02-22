f693d3f0130171c1fe666be34f35be5cca4d77c1
”MarcoRomanoHub” <marcoromano270@gmail.com>
2024-02-22 17:27:39 +0100

    [add] complete

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
+const boxContainer = document.querySelector('.boxContainer');
+
 for(let i = 1; i <= 100; i++){
+
+  const box = document.createElement('div');
+  box.innerHTML = `${i}`;
+  box.classList.add('box');
+  boxContainer.append(box);
+
   if(i % 3 === 0 && i % 5 === 0){
-    console.log('fizzbuzz');
+    box.innerHTML ='fizzbuzz';
+    box.classList.add('fizzbuzz');
   }else if(i % 3 === 0){
-    console.log('fizz');
+    box.innerHTML ='fizz';
+    box.classList.add('fizz');
   }else if(i % 5 === 0){
-    console.log('buzz');
+    box.innerHTML ='buzz';
+    box.classList.add('buzz');
   }else{
     console.log(i);
   }
+
 }
